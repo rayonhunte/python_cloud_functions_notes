d5110507b03a2b4e30b0fb2f216183380c937f71
Rayon Hunte <rayon.hunte@gmail.com>
2023-03-06 10:57:06 -0400

    updated notes

--- gcloud functions deploy python-finalize-.sh
+++ gcloud functions deploy python-finalize-.sh
@@ -22,6 +22,22 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 
   // deploy gen one function
   gcloud functions deploy scoreParser --runtime python310 --trigger-"bucket=gs://gedtsolo.appspot.com" --allow-unauthenticated
+  
+  gcloud functions deploy signUp \
+  --runtime python310 \
+  --trigger-event "providers/cloud.firestore/eventTypes/document.write" \
+  --trigger-resource "projects/gedtsolo/databases/(default)/documents/messages/{pushId}"
+
+  gcloud functions deploy signUp \
+  --runtime python310 \
+  --trigger-event "providers/cloud.firestore/eventTypes/document.write" \
+  --trigger-resource "projects/gedtsolo/databases/(default)/documents/messages/{pushId}"
+
+  gcloud functions deploy signUp \
+  --runtime python310 \
+  --trigger-event "providers/cloud.firestore/eventTypes/document.write" \
+  --trigger-resource "projects/gedtsolo/databases/(default)/documents/scores/{pushId}"
+bck-i-search: deploy_
 
 
   curl localhost:5003 \
@@ -43,3 +59,58 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
         "subscription": "projects/MY-PROJECT/subscriptions/MY-SUB"
       }'
     
+    # run locally 
+    functions-framework --target=scoreParser --signature-type=event --debug --port=5003
+    functions-framework --target=signUp --signature-type=event --debug --port=5003
+
+
+
+     curl localhost:5003 \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -H "ce-id: 123451234512345" \
+  -H "ce-specversion: 1.0" \
+  -H "ce-time: 2020-01-02T12:34:56.789Z" \
+  -H "ce-type: google.cloud.pubsub.topic.v1.messagePublished" \
+  -H "ce-source: //pubsub.googleapis.com/projects/MY-PROJECT/topics/MY-TOPIC" \
+  -d '{
+        "message": {
+            "bucket": "gedtsolo.appspot.com",
+  "contentDisposition": "inline; filename*=utf-8''1677266488328.csv",
+  "contentType": "text/csv",
+  "name": "scores/1677266488328.csv",
+  "size": "2108"
+        },
+        "subscription": "projects/MY-PROJECT/subscriptions/MY-SUB"
+      }'
+
+
+curl -X POST \
+https://api-eval.signnow.com/document \
+-H 'Authorization: Bearer {{MWNlOWIyNjQwNzhiODI1NDNkOTY5Y2RkNDRjMDI2MGY6YWY2ZjM4MWU5NzU0ZjI5YjFjNDY3NGM2ODFmNGQ1MjA}}' \
+-H 'content-type: multipart/form-data' \
+-F file=@GEDT_Master.pdf
+
+curl --request POST \
+  --url https://api-eval.signnow.com/oauth2/token \
+  --header 'Authorization: Basic {{Y2ZjMjAwY2NiZTFhYzZkOWU2NDMzYTdkOWVkZDBkNzM6MGRhN2Q4ZWRhY2I5OTIxMzhjM2M5MWI2ZDUwNjE4ZTE=}}' \
+  --header 'Content-Type: multipart/form-data' \
+  --header 'Content-type: ' \
+  --form username= \
+  --form password= \
+  --form grant_type=password \
+  --form scope= \
+  --form refresh_token= \
+  --form code= \
+  --form expiration_time=
+
+
+
+curl -X POST \
+https://api-eval.signnow.com/document/{{1234}}/invite \
+-H 'Authorization: Bearer {{Y2ZjMjAwY2NiZTFhYzZkOWU2NDMzYTdkOWVkZDBkNzM6MGRhN2Q4ZWRhY2I5OTIxMzhjM2M5MWI2ZDUwNjE4ZTE=}}' \
+-H 'Content-Type: application/json' \
+-d '{
+"from":"sender@signnow.com",
+"to":"signer.email@signnow.com"
+}'
